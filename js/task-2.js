bc144bcde2b55901ed938155192314652c7bdc4f
Artur <134635818+gallesSs@users.noreply.github.com>
2024-03-19 22:16:02 +0200

    hw 6 done

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+class Storage {
+    #items;
+    constructor( items ) {
+        this.#items = items;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        const filteredArray = this.#items.filter(item => item !== itemToRemove);
+        this.#items = filteredArray;
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
